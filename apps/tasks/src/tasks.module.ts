3734079cc9182343bab8be160eab65837ad41cd5
alexander-dobrin <alexander.dobrin@icloud.com>
2023-10-21 11:13:12 +0300

    feat: connect to postgres db

--- apps/tasks/src/tasks.module.ts
+++ apps/tasks/src/tasks.module.ts
@@ -1,9 +1,10 @@
+import { DatabaseModule } from '@app/common/database/database.module';
 import { Module } from '@nestjs/common';
 import { TasksController } from './tasks.controller';
 import { TasksService } from './tasks.service';
 
 @Module({
-  imports: [],
+  imports: [DatabaseModule],
   controllers: [TasksController],
   providers: [TasksService],
 })
